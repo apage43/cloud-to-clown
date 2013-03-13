3ff6beea3b8c69c423019d7f3f8fcd978b3c3f04
Aaron Miller <apage43@ninjawhale.com>
2013-03-12 20:12:40 -0700

    clown computing

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,11 +31,17 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-    v = v.replace(/\bThe Cloud\b/g, "The Clown");
+	v = v.replace(/\bThe Cloud\b/g, "The Clown");
 	v = v.replace(/\bThe cloud\b/g, "The clown");
-    v = v.replace(/\bthe Cloud\b/g, "the Clown");
+	v = v.replace(/\bthe Cloud\b/g, "the Clown");
 	v = v.replace(/\bthe cloud\b/g, "the clown");
-	
+
+
+	v = v.replace(/\bcloud computing\b/g, "clown computing");
+	v = v.replace(/\bCloud computing\b/g, "Clown computing");
+	v = v.replace(/\bcloud Computing\b/g, "clown Computing");
+	v = v.replace(/\bCloud Computing\b/g, "Clown Computing");
+
 	textNode.nodeValue = v;
 }
 
